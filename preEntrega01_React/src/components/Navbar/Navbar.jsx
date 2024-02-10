dc16830a490650225642c656087b3051d97148c6
dos2z <eloy.zaid@gmail.com>
2024-02-10 12:18:22 -0300

    Se crea el entorno de desarrollo para React con Vite. Se construlle el Navbar y se le añaden estilos

---
+++ preEntrega01_React/src/components/Navbar/Navbar.jsx
@@ -0,0 +1,17 @@
+import "./Navbar.css"
+
+const Navbar = () => {
+  
+  return (
+    <nav>
+        <h1>Phoenix Coffee Roaesters</h1>
+        <div className="navbar__menu">
+            <a href="">Café</a>
+            <a href="">Cafeteras</a>
+            <a href="">Molinos</a>
+        </div>
+    </nav>
+  )
+}
+
+export default Navbar
---
+++ preEntrega01_React/src/main.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import Navbar from './components/Navbar/Navbar.jsx'
+import "./reset.css"
+
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <Navbar />
+  </React.StrictMode>,
+)
